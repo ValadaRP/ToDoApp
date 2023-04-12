3ed1438c9f5661c8bd64a191a36708ed0f9e80d5
ValadaRP <s37214@s.pwste.edu.pl>
2023-04-12 14:00:12 +0200

    Added mui material for dialog and created one dialog for deleting todo

--- frontend/src/components/DeleteTodo.tsx
+++ frontend/src/components/DeleteTodo.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {RiDeleteBack2Line} from 'react-icons/ri';
 import axios from 'axios';
 import { useQueryClient, useMutation} from '@tanstack/react-query';
 import { toastSucces } from '../toast/toastFunction';
+import { Dialog } from '@mui/material';
+import './Dialog.scss';
 
 interface IdeleteTodo{
     todoId: number;
+    dialogHeader: string;
 }
 
 const DeleteTodo: React.FC<IdeleteTodo> = (props) => {
     const queryClient = useQueryClient();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpenDialog = () => {
+      setIsOpen(!isOpen);
+    };
 
     const apiCall = async ({id}: {id: number}): Promise<any> => {
         return await axios.post('http://localhost:5000/todo/delete',{
@@ -31,7 +39,16 @@ const DeleteTodo: React.FC<IdeleteTodo> = (props) => {
     }
 
     return (
-      <RiDeleteBack2Line onClick={todoDeleteHandler}/>
+      <>
+        <RiDeleteBack2Line onClick={handleOpenDialog}/>
+        <Dialog open={isOpen} onClose={handleOpenDialog}>
+          <div className='delete_dialog'>
+            <h1>{props.dialogHeader}</h1>
+            <button onClick={todoDeleteHandler}>Yes</button>
+            <button onClick={handleOpenDialog}>No</button>
+          </div>
+        </Dialog>
+      </>
     );
 };
 
