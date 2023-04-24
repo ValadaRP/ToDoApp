2fb0019ec5705c7a1c0105c923d3578a3955a5e5
ValadaRP <s37214@s.pwste.edu.pl>
2023-04-24 12:36:26 +0200

    Changes db client for prisma and changed function to prisma calls

--- backend/src/controllers/toDoController.ts
+++ backend/src/controllers/toDoController.ts
@@ -1,34 +1,49 @@
 import {RequestHandler} from 'express';
-import { DBQuery,DBQueryParam, DBUpdate, DBCreate, DBDelete } from '../models/DBConnect';
 import { Todo } from '../models/todo';
 import { HttpError } from '../models/http-error';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 interface todo{
-    Id:number;
-    Name:string;
-    Content: string;
-    Date: Date;
+    id:number;
+    name:string;
+    content: string;
+    completed: boolean;
+    createdAt: Date;
+
 }
 
 export const getTodo: RequestHandler = async (req,res,next) => {
-    // const rows = await DBQueryParam(`SELECT * FROM todo where id = ?`, 1);
-
-    const rows = await DBQuery("SELECT * FROM todo");
-    res.status(201).json({todo: rows});
+    const todos = await prisma.todo.findMany();
+    prisma.$disconnect();
+    res.status(201).json({todo: todos});
 };
 
 export const getTodoById: RequestHandler<{id: number}> = async (req,res,next) => {
     const todoId = req.params.id;
-    const response: todo = await DBQueryParam(`SELECT * FROM todo where id = ?`,todoId);
-    console.log(response.Id);
-
-    res.status(201).json({message: `Your id is ${todoId}`, res: response});
+    const todo = await prisma.todo.findFirst({
+        where: {
+            id: todoId
+        }
+    });
+    prisma.$disconnect();
+    res.status(201).json({message: `Your id is ${todoId}`, res: todo});
 }
 
 export const updateTodo: RequestHandler = async (req,res,next) => {
     // const todoId = (req.body as {id:number}).id;
     const {id, name, content} = req.body as {id:number, name: string, content: string};
-    await DBUpdate(`UPDATE todo SET Name = ?, Content = ? WHERE Id = ?`,id,name, content);
+    await prisma.todo.update({
+        where: {
+            id: id
+        },
+        data: {
+            name: name,
+            content: content
+        }
+    });
+    prisma.$disconnect();
     res.status(201).json({message: `You have sucesfully updated todo: ${id}`});
 }
 
@@ -36,18 +51,23 @@ export const createTodo: RequestHandler = async (req,res,next) => {
     const { name, content } = req.body as {name: string, content: string};
 
     const todo: Todo = new Todo(name, content);
-    await DBCreate(`INSERT INTO todo (Name, Content) VALUES (?, ?);`,todo.name, todo.content);
-
+    await prisma.todo.create({data: {name: todo.name, content: todo.content}});
+    
+    await prisma.$disconnect();
+    
     res.status(201).json({message: `Your new todo is ${todo}`});
 }
 
 export const delteTodo: RequestHandler = async (req, res, next) => {
     const {id} = req.body as {id: number};
-    const todo: todo = await DBQueryParam(`SELECT * FROM todo where id = ?`, id);
-    if(!todo){
-        return next(new HttpError("Todo with this id doesn't exist", 400));
-    }
-    await DBDelete(`DELETE from todo WHERE id = ?`,id);
-
+    // if(!todo){
+    //     return next(new HttpError("Todo with this id doesn't exist", 400));
+    // }
+    await prisma.todo.delete({
+        where: {
+            id: id
+        }
+    });
+    await prisma.$disconnect();
     res.status(201).json({message: "You have sucesfully deleted todo"});
 }
